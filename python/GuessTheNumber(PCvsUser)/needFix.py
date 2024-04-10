3935b480e41a1f3cc593fa0e63125d6d425f4ce7
Guilherme Carvalho <guilhermecarvalho9205@gmail.com>
2024-04-10 18:17:35 -0300

    Need fix code

--- python/GuessTheNumber(PCvsUser)/needFix.py
+++ python/GuessTheNumber(PCvsUser)/needFix.py
@@ -16,34 +16,28 @@ while N <= 5:
     rangeVerify(userNumber)
     
         
-numberRepeated = userNumber
-while N >= 1 and numberRepeated == userNumber or userNumber not in range(1, 11):
+    numberRepeated = userNumber
+    while N >= 1 and numberRepeated == userNumber or userNumber not in range(1, 11):
         userNumber = int(input("Este número já foi selecionado ou não está entre 1 e 10. Escolha outro: ")) 
         numberChoosed.append(userNumber)
 
-for x in numberChoosed:
-    if x in repeatedList:
-        userNumber = int(input("Este número já foi selecionado anteriormente. Selecione outro: "))
-    elif x not in repeatedList:
-        repeatedList.append(x)
             
-            
-print(f"\nNúmero escolhido por IA: {randomNumber}")
-print(f"Número escolhiodo pelo usuário: {userNumber}")
+    print(f"\nNúmero escolhido por IA: {randomNumber}")
+    print(f"Número escolhiodo pelo usuário: {userNumber}")
 
-if randomNumber == userNumber:
+    if randomNumber == userNumber:
         print("Empate!")
-elif randomNumber > userNumber:
+    elif randomNumber > userNumber:
         print("Ponto para a IA!")
         score1 +=1
-elif randomNumber < userNumber:
+    elif randomNumber < userNumber:
         print("Ponto para o usuário!")
         score2 +=1
 
-print(f"\nTotal de pontos da IA: {score1}")
-print(f"Total de pontos do usuário: {score2}")
-print(f"\nOs números já digitados pelo usuário são: {repeatedList}")
-N += 1
+    print(f"\nTotal de pontos da IA: {score1}")
+    print(f"Total de pontos do usuário: {score2}")
+    print(f"\nOs números já digitados pelo usuário são: {numberChoosed}")
+    N += 1
 if N == 6 and score1 == score2:
     print("O jogo deu empate! Vamos recomeçar: \n\n")
     N = 0
